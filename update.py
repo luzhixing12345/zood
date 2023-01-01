f874cd2a04c25d48a569ee1025ed52e4d738eff6
luzhixing12345 <luzhixing12345@163.com>
2023-01-01 18:57:47 +0800

    [perf] : update css

--- update.py
+++ update.py
@@ -15,7 +15,7 @@ with open('pyproject.toml','r') as f:
 version = file[2]
 version_number = re.findall(r"\d+\.?\d*",version)[1]
 new_version_number = int(version_number) + 1
-file[2] = f'version = "0.0.{new_version_number}"\n'
+file[2] = f'version = "0.1.{new_version_number}"\n'
 
 print("version = ",new_version_number)
 
