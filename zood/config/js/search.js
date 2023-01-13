65bd950960395cd07b41c765020ec92e85545b92
luzhixing12345 <luzhixing12345@163.com>
2023-01-13 20:38:07 +0800

    [perf] : change some css

--- zood/config/js/search.js
+++ zood/config/js/search.js
@@ -1,12 +1,23 @@
 
-
 var center_search_div = document.createElement('div')
 center_search_div.className = 'center-search'
+
+// header
+var center_search_header = document.createElement('div')
+center_search_header.className = 'search-header'
+
+var center_search_img = document.createElement('img')
 var center_search_input = document.createElement('input')
+center_search_input.placeholder = '请输入查询内容'
+
+center_search_header.appendChild(center_search_input)
+center_search_header.appendChild(center_search_img)
+
+// search result
 var search_result = document.createElement('div')
 search_result.className = 'search-result'
 
-center_search_div.appendChild(center_search_input)
+center_search_div.appendChild(center_search_header)
 center_search_div.appendChild(search_result)
 document.body.appendChild(center_search_div)
 
@@ -31,15 +42,19 @@ function closeCenterSearch() {
     center_search_div.style.display = 'none';
     black_overlay.style.display = 'none';
     is_display = !is_display;
+    center_search_input.value = '';
 }
 
 
-function startSearch(params) {
+function startSearch(word) {
     
 }
 
 
 function addSearchBar(search_src,key_map) {
+
+    center_search_img.src = search_src;
+
     var search_bar = document.createElement('div');
     search_bar.className = 'search-bar';
     var search_img = document.createElement('img');
@@ -62,5 +77,13 @@ document.onkeydown = function (e) {
     }
     if(ev.keyCode == 27){
         closeCenterSearch()
+    }
+    if (ev.which === 13) {
+        var word = center_search_input.value.trim();
+        if ( word== '') {
+            closeCenterSearch()
+            return;            
+        }
+        startSearch(word)
     }
 }
