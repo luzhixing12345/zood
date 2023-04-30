3add381c692c12200144fcf1f3dd26c719cb43d2
luzhixing12345 <luzhixing12345@163.com>
2023-05-01 03:12:08 +0800

    [feat] : add naviagtor bold when scroll into view

--- zood/config/js/navigator.js
+++ zood/config/js/navigator.js
@@ -7,6 +7,34 @@ navigator_links.forEach(link => {
     link.addEventListener('click', function (event) {
         event.preventDefault();
         let target = document.querySelector(this.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth',block: 'start', inline: 'nearest'});
+        target.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
     });
+});
+
+function isScrolledIntoView(elem) {
+    var docViewTop = window.pageYOffset;
+    var docViewBottom = docViewTop + window.innerHeight;
+    var elemTop = elem.offsetTop;
+    var elemBottom = elemTop + elem.offsetHeight;
+    return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
+}
+var headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+window.addEventListener('scroll', function () {
+    var found = false;
+    for (var heading of headings) {
+        if (!found && isScrolledIntoView(heading)) {
+            var heading_id = heading.id;
+            var link = document.querySelector(`a[href="#${heading_id}"]`);
+            if (link) {
+                link.style.fontWeight = 'bold';
+                found = true;
+            }
+        } else {
+            var heading_id = heading.id;
+            var link = document.querySelector(`a[href="#${heading_id}"]`);
+            if (link) {
+                link.style.fontWeight = 'normal';
+            }
+        }
+    }
 });
